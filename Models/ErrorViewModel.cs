7429cecef3d42cf009b166debffd6f72d5883863
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2022-05-31 18:24:04 +0200

    Aggiungere i file di progetto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace la_mia_pizzeria_static_02.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
